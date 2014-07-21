333bf99693ca75449e6cadf31ac4d452b268b7d3
Mihai Scurtu <mscurtu90@gmail.com>
2014-07-21 17:50:10 +0300

    Add basic expansion behaviour

--- js/infinitable.jquery.js
+++ js/infinitable.jquery.js
@@ -1,10 +1,25 @@
 (function($) {
 	$.fn.infiniTable = function() {
-		if(!this.is('table')) {
+		var _this = this;
+		if(!_this.is('table')) {
 			$.error('Subject is not a table');
 		}
 
-		init(this);
+		init(_this);
+
+		_this.on('focus', 'input', function(e) {
+			console.log(_this.data(), $(this).parent().data());
+
+			if($(this).parent().data('x') == _this.data('cells') - 1) {
+				expandTable(_this, 'x');
+			}
+
+			if($(this).parent().data('y') == _this.data('rows') - 1) {
+				expandTable(_this, 'y');
+			}
+
+			// console.log(_this.data());
+		});
 
 		return this;
 	}	
@@ -20,21 +35,77 @@
 			rows = table.find('tr');
 		}
 
+		// save table size
+		table.data({
+			rows: table.find('tr').size(),
+			cells: table.find('tr:first').find('td').size()
+		});
+
+
 		// init cells
 		$.each(rows, function(i){
 			$.each($(this).find('td'), function(j) {
-				// add coords
-				$(this).data({
-					x: i,
-					y: j
-				});
-
-				// add input box
-				if($(this).find('input').size() == 0) {
-					var text = $(this).text();
-					$(this).empty().append($('<input type="text">').val(text));
-				}
+				initCell($(this), j, i);
 			});
+		});
+	}
+
+	/**
+	 * Initialize a cell
+	 * @param  {$} cell The cell jqery object
+	 * @param  {int} x    The cell's x coordinate
+	 * @param  {int} y    The cell's y coordinate
+	 */
+	var initCell = function(cell, x, y) {
+		// add coords
+		cell.data({
+			x: x,
+			y: y
+		});
+
+		// add input box
+		if(cell.find('input').size() == 0) {
+			var text = cell.text();
+			cell.empty().append($('<input type="text">').val(text));
+		}
+	}
+
+	var expandTable = function(table, dir) {
+		var newCells = table.data('cells');
+		var newRows = table.data('rows');
+
+		if(dir == 'x') {
+			newCells += 1;
+
+			// add a new cell to each row
+			table.find('tr').each(function(i) {
+				var cell = $('<td>');
+				initCell(cell, newCells - 1, i);
+
+				$(this).append(cell);
+			});
+		}
+
+		if(dir == 'y') {
+			newRows += 1;
+
+			// create a new row
+			var row = $('<tr>');
+
+			for(i = 0; i < newCells; i++) {
+				var cell = $('<td>');
+				initCell(cell, i, newRows - 1); 
+
+				row.append(cell);
+			}
+
+			table.append(row);
+		}
+
+		// update table size
+		table.data({
+			cells: newCells,
+			rows: newRows
 		});
 	}
 }(jQuery));
