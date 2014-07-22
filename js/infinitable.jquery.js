d8b935b01928ef49bbc5f60cd7bae725ccfe2610
Mihai Scurtu <mscurtu90@gmail.com>
2014-07-22 17:54:22 +0300

    Add names to inputs

--- js/infinitable.jquery.js
+++ js/infinitable.jquery.js
@@ -70,7 +70,8 @@
 		// add input box
 		if(cell.find('input').size() == 0) {
 			var text = cell.text();
-			cell.empty().append($('<input type="text">').attr('maxlength', 2).val(text));
+			cell.empty().append($('<input type="text">').attr('maxlength', 2)
+				.attr('name', 'data['+x+']['+y+']').val(text));
 		}
 
 		if(table.data && table.data('cells')) {
