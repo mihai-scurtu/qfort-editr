53367c7be90d5d180cf7c4d224d3a827f8792a8c
Mihai Scurtu <mscurtu90@gmail.com>
2014-07-23 12:34:23 +0300

    Fix empty comment

--- download.php
+++ download.php
@@ -3,6 +3,11 @@ require_once 'functions.php';
 
 if(is_array($_POST['data'])) {
 	$data = array_trim($_POST['data']);
+
+	if($_POST['comment'] == '#') {
+		$_POST['comment'] = false;
+	}
+
 	$comment = $_POST['comment'] ? $_POST['comment'] : '#qfort-editr-blueprint';
 	$filename = str_replace(array('#', ' '), array('', '-'), $comment).'.csv';
 
