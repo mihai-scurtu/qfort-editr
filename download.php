1f42181d52e06b5cb7649d6e0097af68e7065e5f
Mihai Scurtu <mscurtu90@gmail.com>
2014-07-23 12:22:18 +0300

    Send file for download

--- download.php
+++ download.php
@@ -1,10 +1,17 @@
 <?php
 require_once 'functions.php';
 
-// var_dump($_POST['data']);
-
 if(is_array($_POST['data'])) {
 	$data = array_trim($_POST['data']);
+	$comment = $_POST['comment'] ? $_POST['comment'] : '#qfort-editr-blueprint';
+	$filename = str_replace(array('#', ' '), array('', '-'), $comment).'.csv';
+
+	if(count($data)) {
+		header('Content-type: text/csv');
+		header('Content-Disposition: attachment; filename="'.$filename.'"');
+
+		echo $_POST['comment'].PHP_EOL.array2csv($data);
+	}
 }
 
 // if(!headers_sent()) {
